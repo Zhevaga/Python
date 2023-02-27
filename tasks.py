4ef5c9d14de3230883371dc124ea4ddc8675c047
Zhevaga <a.zhevaga@mail.ru>
2023-02-27 23:00:33 +0400

    task_of_strings

--- tasks.py
+++ tasks.py
@@ -15,3 +15,32 @@ for i in range(a, b + 1):
         max_num = i
     total = 0
 print(max_num, max_total)
+
+""" На вход программе подается одна строка. Напишите программу, 
+которая выводит сообщение «Цифра» (без кавычек), если строка содержит цифру.
+В противном случае вывести сообщение «Цифр нет» (без кавычек)."""
+text = input()
+flag = 'Цифр нет'
+for i in range(len(text)):
+    for j in range(0, 10):
+        if str(j) == text[i]:
+            flag = 'Цифра'
+            break
+print(flag)
+
+"""На вход программе подается строка состоящая из имени и фамилии человека, 
+разделенных одним пробелом. Напишите программу, которая проверяет, что имя и фамилия 
+начинаются с заглавной буквы."""
+name = input()
+i = 0
+while i < len(name):
+    if name[i] in ' ':
+        num = i
+        break
+    else:
+        i += 1
+if name[0] == name[0].upper() and name[num + 1] == name[num + 1].upper():
+    print('YES')
+else:
+    print('NO')
+    
