d04c97666481d68cf7904c930fa1a9f6b99a423b
Zhevaga <a.zhevaga@mail.ru>
2023-04-15 22:41:30 +0400

    sum_mult_matrix

--- task_course2.py
+++ task_course2.py
@@ -27,6 +27,7 @@ for i in range(len(code)):
 n. Напишите программу, которая выводит первые
 n строк треугольника Паскаля."""
 
+
 def pascal(n):
     list1 = []
     for i in range(n + 1):
@@ -38,6 +39,7 @@ def pascal(n):
                 list2.append(list1[i-1][j-1] + list1[i-1][j])
         list1.append(list2)
     return list1
+
 
 n = int(input())
 
@@ -55,7 +57,7 @@ matrix = []
 for i in range(n):
     num = [j for j in input().split()]
     matrix.append(num)
-    
+
 new_matrix = []
 for i in range(n):
     new_row = []
@@ -88,15 +90,225 @@ matrix = []
 for k in range(8):
     elem = ['.' for _ in range(8)]
     matrix.append(elem)
-    
+
 matrix[i][j] = 'N'
 
 for h in range(8):
     for g in range(8):
-        if abs(h - i) == 2 and abs(g - j) == 1 or abs(g - j) == 2 and abs(h - i) == 1:
+        if (abs(h - i) == 2 and abs(g - j) == 1) or (abs(g - j) == 2
+                                                     and abs(h - i) == 1):
             matrix[h][g] = '*'
 
 for c in range(8):
     for r in range(8):
         print((matrix[c][r]), end=' ')
     print()
+
+"""На вход программе подаются два натуральных числа n и m. Напишите программу,
+которая создает матрицу размером n×m заполнив её в соответствии с образцом.
+Input: 7 3
+Output:
+1 2 3
+2 3 1
+3 1 2
+1 2 3
+2 3 1
+3 1 2
+1 2 3
+"""
+num = input().split()
+m = int(num[0])
+n = int(num[-1])
+matrix = []
+list_elem = [i for i in range(1, n + 1)]
+
+for i in range(m):
+    row = []
+    if i >= n:
+        index = i - n - 1
+    else:
+        index = i - 1
+    for j in range(n):
+        index += 1
+        while index >= n:
+            index -= n
+        elem = list_elem[index]
+        row.append(elem)
+    matrix.append(row)
+
+
+for i in range(m):
+    for j in range(n):
+        print(str(matrix[i][j]).ljust(3), end='')
+    print('')
+
+"""На вход программе подаются два натуральных числа n и m. Напишите программу,
+которая создает матрицу размером n×m заполнив её "диагоналями" в соответствии
+с образцом."""
+num = input().split()
+m = int(num[0])
+n = int(num[-1])
+matrix = []
+elem = 0
+
+list_n = [i + 1 for i in range(n * m)]
+
+new_matrix = [[0] * n for _ in range(m)]
+
+c = 0
+count = 1
+for r in range(n + m):
+    if m < n:
+        if r < n:
+            i = r
+            for j in range(i + 1):
+                if j < m:
+                    new_matrix[j][i] = list_n[c]
+                    i -= 1
+                    c += 1
+                else:
+                    break
+        else:
+            i = r - count
+            for j in range(count, i + 1):
+                if j < m:
+                    new_matrix[j][i] = list_n[c]
+                    i -= 1
+                    c += 1
+                else:
+                    break
+            count += 1
+    else:
+        if r < n:
+            i = r
+            for j in range(i + 1):
+                if j < n:
+                    new_matrix[j][i] = list_n[c]
+                    i -= 1
+                    c += 1
+                else:
+                    break
+        else:
+            i = r - count
+            for j in range(count, r + 1):
+                if j < m:
+                    new_matrix[j][i] = list_n[c]
+                    i -= 1
+                    c += 1
+                else:
+                    break
+            count += 1
+
+for i in range(m):
+    for j in range(n):
+        print(str(new_matrix[i][j]).ljust(3), end='')
+    print()
+
+"""На вход программе подаются два натуральных числа n и m. Напишите программу,
+которая создает матрицу размером n×m заполнив её "спиралью" в соответствии
+с образцом.
+Sample Input 1:
+4 5
+Sample Output 1:
+1  2  3  4  5
+14 15 16 17 6
+13 20 19 18 7
+12 11 10 9  8
+Sample Input 2:
+1 6
+Sample Output 2:
+1  2  3  4  5  6
+"""
+num = input().split()
+n = int(num[0])
+m = int(num[-1])
+matrix = []
+matrix = [[0] * m for _ in range(n)]
+
+elem = 1
+i = 0
+j = 0
+r = n
+s = m
+count = 0
+
+if m == 1 or n == 1:
+    for i in range(n):
+        for j in range(m):
+            matrix[i][j] = elem
+            elem += 1
+else:
+    while elem <= n * m:
+        while j < s - 1:
+            matrix[i][j] = elem
+            j += 1
+            elem += 1
+        while i < r:
+            matrix[i][j] = elem
+            i += 1
+            elem += 1
+        i -= 1
+        j -= 1
+        while count < j:
+            matrix[i][j] = elem
+            j -= 1
+            elem += 1
+        while count < i:
+            matrix[i][j] = elem
+            i -= 1
+            elem += 1
+        i += 1
+        j += 1
+        s -= 1
+        r -= 1
+        count += 1
+
+for i in range(n):
+    for j in range(m):
+        print(str(matrix[i][j]).ljust(3), end='')
+    print()
+
+"""На вход программе подаются два натуральных числа n и m — количество строк и
+столбцов в первой матрице, затем элементы первой матрицы, затем пустая строка.
+Далее следуют числа m и k — количество строк и столбцов второй матрицы затем
+элементы второй матрицы."""
+num1 = input().split()
+n = int(num1[0])
+m = int(num1[-1])
+
+matrix1 = [[int(i) for i in input().split()] for _ in range(n)]
+
+st = input()
+
+num2 = input().split()
+m = int(num2[0])
+k = int(num2[-1])
+
+matrix2 = [[int(i) for i in input().split()] for _ in range(m)]
+
+matrix3 = [[0] * k for _ in range(n)]
+for i in range(n):
+    for j in range(k):
+        for x in range(m):
+            matrix3[i][j] += matrix1[i][x] * matrix2[x][j]
+
+for row in matrix3:
+    print(*row)
+
+"""Напишите программу, которая возводит квадратную матрицу в m-ую степень."""
+n = int(input())
+matrix = [[int(i) for i in input().split()] for _ in range(n)]
+m = int(input())
+
+matrix_res = matrix.copy()
+
+for _ in range(m - 1):
+    matrix1 = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for x in range(n):
+                matrix1[i][j] += matrix[i][x] * matrix_res[x][j]
+    matrix = matrix1.copy()
+
+for row in matrix1:
+    print(*row)
